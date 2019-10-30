cec17638e20b86befff429c07c1a760705254269
Christophe van Baalen <chichoch@gmail.com>
2019-10-30 11:02:50 +0100

    Made it prettier

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,8 @@ export default function Footer(props) {
   const [cmd, setCmd] = useState("");
 
   function handleOnChange(event) {
-    setCmd(event.target.value);
+    const cmd = event.target.value;
+    setCmd(cmd.slice(3, cmd.length));
   }
 
   function handleSubmit(event) {
@@ -17,7 +18,12 @@ export default function Footer(props) {
   return (
     <footer className="footer">
       <form onSubmit={handleSubmit}>
-        <input type="text" value={cmd} onChange={handleOnChange} />
+        <input
+          type="text"
+          value={"$> " + cmd}
+          onChange={handleOnChange}
+          autoFocus
+        />
       </form>
     </footer>
   );
