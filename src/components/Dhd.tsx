92d28e62f9ebdd0ca624e530e3ac76cc4de4c3e7
Christophe van Baalen <chichoch@gmail.com>
2026-03-03 00:03:18 +0100

    Fixed broken build

---
+++ src/components/Dhd.tsx
@@ -0,0 +1,10 @@
+export default function Dhd() {
+  return (
+    <iframe
+      title="Denim by Desire and Her Drunks"
+      style={{ border: 0, width: 350, height: 470 }}
+      src="https://bandcamp.com/EmbeddedPlayer/album=2623330495/size=large/bgcol=333333/linkcol=e99708/tracklist=false/transparent=true/"
+      seamless
+    ></iframe>
+  );
+}
